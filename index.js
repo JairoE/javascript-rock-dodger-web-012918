6e65a6ca1dd03167c7c8d570a98539e116c48ea3
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-18 18:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -121,6 +121,14 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  gameInterval = null 
+  ROCKS = [] 
+  rocks = document.getElementsByClassName('rock')
+  for (rock of rocks){
+    GAME.removeChild(rock)
+  }
+  
+  alert("YOU LOSE!")
 }
 
 function moveDodger(e) {
