b09a3ed737f707155675c59c266f950e42e8f616
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-18 19:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ function moveDodgerRight() {
    let dodgerLeftEdge = positionToInteger(DODGER.style.left)
    let dodgerRightEdge = positionToInteger(DODGER.style.right)
   function stepright(){
-    if (dodgerRightEdge < 396){
+    if (dodgerRightEdge < 356){
      DODGER.style.left = `${dodgerLeftEdge +=4}px`
      DODGER.style.right = `${dodgerRightEdge +=4}px`
     }
